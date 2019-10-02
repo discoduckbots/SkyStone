6ac9d5dc6bc302aff7023a8629576030b5716f46
Binu Abraham <binua2@verifone.com>
2019-10-02 00:23:10 -0500

    Fixed encoder functions better
    Fixed encoding functions according to instructions at
    https://stemrobotics.cs.pdx.edu/node/4746

--- DiscoDuckBots/src/main/java/org/firstinspires/ftc/discoduckbots/hardware/MecanumMotor.java
+++ DiscoDuckBots/src/main/java/org/firstinspires/ftc/discoduckbots/hardware/MecanumMotor.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.discoduckbots.hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.discoduckbots.util.NumberUtility;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -64,7 +65,7 @@ public class MecanumMotor {
     }
 
     /**
-     * This function stops the mechanum motor
+     * This function stops the mecanum motor
      */
     public void stop() {
         mFrontLeft.setPower(0);
@@ -79,43 +80,89 @@ public class MecanumMotor {
 
     /**
      * This function moves the mecanum motor forward or backward by specifying the distance to move
+     * After calling this function make sure to add the following to wait for the
+     * robot to finish moving
+     * while (opModeIsActive() && mecanumMotor.isBusy())  {
+     *     idle();
+     * }
      * @param distance - positive value in inches to move forward
      *                 - negative value in inches to move backwards
      */
-    public void driveByDistance(int distance) {
+    public void driveByDistance(int power, int distance) {
         int direction = 1;
         // if the distance is negative, move all the wheels in opposite direction
         if (distance < 1) direction = -1;
 
         // convert the absolute distance to number of rotations
         int target = convertDistanceToTarget(Math.abs(distance));
+        mTelemetry.addData("driveByDistance: ", "target: ", target);
 
-        mFrontLeft.setTargetPosition(target * direction);
-        mFrontRight.setTargetPosition(target * direction);
-        mBackLeft.setTargetPosition(target * direction);
-        mBackRight.setTargetPosition(target * direction);
+        // set the power of the motors according to the direction they need to go to straf
+        mFrontLeft.setPower(power * direction );
+        mFrontRight.setPower(power * direction);
+        mBackLeft.setPower(power * direction);
+        mBackRight.setPower(power * direction);
+
+        // set all motors except the front left to go without encoder
+        mFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // use the front left - just picked random to measure the rotations
+        // and set the rotations to the distance to go
+        mFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        mFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        mFrontLeft.setTargetPosition(target);
     }
 
     /**
      * This function moves the mecanum motor sideways by specifying the distance to move
+     * After calling this function make sure to add the following to wait for the
+     * robot to finish moving
+     * while (opModeIsActive() && mecanumMotor.isBusy())  {
+     *   idle();
+     * }
      * @param distance - positive value in inches to straf to the right
      *                 - negative value in inches to straf to the left
      */
-    public void strafByDistance(int distance) {
+    // https://stemrobotics.cs.pdx.edu/node/4746
+    public void strafByDistance(int power, int distance) {
         int direction = 1;
         // if the distance is negative, move all the wheels in opposite direction
         if (distance < 1) direction = -1;
 
         // convert the absolute distance to number of rotations
         int target = convertDistanceToTarget(Math.abs(distance));
+        mTelemetry.addData("strafByDistance: ", "target: ", target);
 
-        mFrontLeft.setTargetPosition(target * direction);
-        mFrontRight.setTargetPosition(target * direction * (-1));
-        mBackLeft.setTargetPosition(target * direction * (-1));
-        mBackRight.setTargetPosition(target * direction);
+        // set the power of the motors according to the direction they need to go to straf
+        mFrontLeft.setPower(power * direction );
+        mFrontRight.setPower(power * direction * (-1));
+        mBackLeft.setPower(power * direction * (-1));
+        mBackRight.setPower(power * direction);
+
+        // set all motors except the front left to go without encoder
+        mFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // use the front left - just picked random to measure the rotations
+        // and set the rotations to the distance to go
+        mFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        mFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        mFrontLeft.setTargetPosition(target);
     }
 
     private int convertDistanceToTarget(int distance) {
         return (int)(distance / (Math.PI * WHEEL_DIAMETER));
+    }
+
+    /**
+     * Returns if atleast one of the wheels is moving
+     * @return
+     */
+    public boolean isMoving() {
+        // TODO should this be all && or || ?
+        return mFrontLeft.isBusy() || mFrontRight.isBusy() || mBackRight.isBusy() || mBackLeft.isBusy();
     }
 }
